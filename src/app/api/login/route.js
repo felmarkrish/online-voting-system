90ce4e76235abae51a5160835b1ee2a093c1835d
felmarkrish <felmarkrish@gmail.com>
2025-09-25 11:11:51 +0800

    redeploy fix naming lib/firebaseadmin

--- src/app/api/login/route.js
+++ src/app/api/login/route.js
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import { db } from "@/lib/firebaseAdmin"; // make sure this uses admin SDK
+import { db } from "@/lib/firebase"; // make sure this uses admin SDK
 
 export async function POST(req) {
   try {
